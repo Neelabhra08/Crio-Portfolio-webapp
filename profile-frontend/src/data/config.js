d2ee3203868b020cbc06456029627e006027a96e
Neelabhra Bhattacharyya <neelabhra08@gmail.com>
2021-01-22 13:03:35 +0530

    Completed and deployed

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Neelabhra Bhattacharyya",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Neelabhra Bhattacharyya and I'm a Full Stack Web Developer!",
+  githubUrl: "https://github.com/Neelabhra08",
+  linkedinUrl: "https://www.linkedin.com/in/neelabhra-bhattacharyya-5a3396176/",
+  cvLink: "https://drive.google.com/file/d/1u51ANvqTk_DvogY1GAlH2ceOrqHYzr0x/view?usp=sharing",
+};
